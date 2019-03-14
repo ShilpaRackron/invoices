12dde6a6a428d2cec5f1a6634a206d6386cc65d9
Jarnail <mtkumar82@gmail.com>
2019-03-14 20:13:01 +0530

    modified files

--- common/config/main.php
+++ common/config/main.php
@@ -11,14 +11,6 @@ return [
         ],
 		'mailer' => [
             'class' => yii\swiftmailer\Mailer::class,
-            /* 'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.gmail.com', //'sipuk2-20.nexcess.net',
-                'username' => 'amazoninvoices2019@gmail.com', //'amazoninvoices@invoices.arsgate.com',
-                'password' => 'MoronsMythicFadedKapok40',
-                'port' => '587',
-                'encryption' => 'tls',
-            ], */
         ],
     ],
 ];
